14bba240196fc42f99d73f33ffe600bc139660eb
Caffrey <sunhongwei0913@163.com>
2026-07-15 16:12:57 +0800

    Update wesafechat to 0.2.2

--- Formula/wesafechat.rb
+++ Formula/wesafechat.rb
@@ -1,10 +1,10 @@
 class Wesafechat < Formula
   desc "macOS menu bar tool to auto-hide WeChat for privacy"
   homepage "https://github.com/CaffreySun/wesafechat"
-  url "https://github.com/CaffreySun/wesafechat/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "91b83bb598030b0054551927864a4c71ab2652f7c6c452ab8bf822c7996230c7"
+  url "https://github.com/CaffreySun/wesafechat/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "432d4be43f2097ac721540af4cf1e5d44c5e23a4ecfcadd05b77c476a33bf2f1"
   license "MIT"
-  version "0.2.1"
+  version "0.2.2"
 
   depends_on xcode: "15.0"
   depends_on macos: :ventura
